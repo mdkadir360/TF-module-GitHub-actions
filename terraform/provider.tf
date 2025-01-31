12bb2efc49ff44315f6351b938b6475de8b5e004
Abdul Kadir <101203829+mdkadir360@users.noreply.github.com>
2025-02-01 00:42:31 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,11 +4,4 @@ provider "azurerm" {
   }
   subscription_id = "7fd59e8b-ebe2-47cb-9c43-38fb4761eff1"
 }
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "myrg"
-    storage_account_name = "kadirstorage25"
-    container_name       = "vm-container"
-    key                  = "vm.terraform.tfstate"
-  }
-}
+#
